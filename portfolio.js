957f9593ab248a2826904e671e956eabc49c9f6b
AlexPine <alexlight.pineapple@gmail.com>
2023-07-21 02:55:51 -0400

    Add set up for changing a header element's position

--- portfolio.js
+++ portfolio.js
@@ -1,25 +1,35 @@
-
+let moveInput = 100;
+let emptyWidth = 0;
+//pageNumber = 0;
 
 windowResize();
+headerWidth(0);
+moveSelector(moveInput);
+
+const pages = [];
+pages[0] = ['EMPTY PAGE','About', 'Skills', 'Projects', 'Contact'];
+pages[1] = [50, 100, 150, 200];
+console.log(pages)
 
 function windowResize() {
     height = innerHeight;
-    //console.log(height)
+    console.log(height)
 }
 
-function isInViewport(element, height) {
+function isInViewport(element, height, array) {
     let bounding = element.getBoundingClientRect();
     let location = bounding.top/height;
-    console.log(`Percentage: ${location}`)
+    //console.log(`Percentage: ${location}`)
+
+
+
 
     if (location > 0.4) {
         page = document.querySelector("#page2");
-        console.log("About");
-        pageName = 'About'
+        pageNumber = 1;
     } else if (location < 0.4 && location > -0.6) {
         page = document.querySelector("#page2");
-        console.log("Skills");
-        pageName = 'Skills'
+        pageNumber = 2;
     } else if (location < -0.6) {
         element = document.querySelector("#page3");
         //console.log("QSELECT PAGE 3")
@@ -27,32 +37,40 @@ function isInViewport(element, height) {
         location = bounding.top/height;
 
         if (location < 0.4 && location > -0.6) {
-            console.log("Projects")
-        pageName = "Projects";
+        pageNumber = 3;
         } else {
-            console.log("Contact")
-        pageName = "Contact";
+        pageNumber = 4;
         }
 
     }
+    console.log(pages[0][pageNumber])
 
-    document.getElementById("header-selector").innerHTML = pageName
+    document.getElementById("header-selector").innerHTML = pages[0][pageNumber];
+    moveSelector(pages[1][pageNumber], pageNumber);
 }
 
 
 
+function headerWidth () {
+   const contentWidth = document.getElementById("header-content").offsetWidth;
+   console.log(contentWidth);
+   const linksWidth = document.querySelector("#header-content .links").offsetWidth;
+   console.log(linksWidth);
+   emptyWidth = contentWidth-linksWidth-100;
+}
+
+function moveSelector (moveInput, page) {
+    let moveTotal = emptyWidth + moveInput;
+    const link = document.querySelector(`a[href="#page${page}"]`);
+    
+    //console.log(link);
+    console.log(moveTotal);
+}
+
 let page = document.querySelector("#page2");
+const headerContent = document.getElementById("header-content");
 
 window.addEventListener("resize", windowResize);
+window.addEventListener("resize", () => moveSelector(headerWidth(), pageNumber));
 
-document.addEventListener("scroll", () => isInViewport(page, height));
-
-const headerButtons = [l0, l1, l2, l3];
-
-
-
-/*
-
-
-
-*/
+document.addEventListener("scroll", () => isInViewport(page, height, pages));
