7205c4c4374685ed6d294c2b7687074dfb2ccaae
AlexPine <alexlight.pineapple@gmail.com>
2023-07-23 19:18:51 -0400

    Rework and animate header

--- portfolio.js
+++ portfolio.js
@@ -1,52 +1,60 @@
-let moveInput = 100;
-let emptyWidth = 0;
-//pageNumber = 0;
+const slider = document.querySelector("#header-selector");
+console.log(slider);
 
 windowResize();
-headerWidth(0);
-moveSelector(moveInput);
-
-const pages = [];
-pages[0] = ['EMPTY PAGE','About', 'Skills', 'Projects', 'Contact'];
-pages[1] = [50, 100, 150, 200];
-console.log(pages)
+headerWidth();
+moveSelector("#page0");
+const pages = document.querySelectorAll('.header ul a');
 
 function windowResize() {
     height = innerHeight;
     console.log(height)
 }
 
+let page = document.querySelector("#page1");
+current = "#page0";
+
 function isInViewport(element, height, array) {
+    //console.log(element);
+
     let bounding = element.getBoundingClientRect();
-    let location = bounding.top/height;
+    //console.log(`"${current}"`)
+    const inViewport = document.querySelector(`${current}`);
+    //console.log(inViewport);
+    let location = bounding.top/inViewport.offsetHeight;
+
+    console.log(location);
     //console.log(`Percentage: ${location}`)
 
-
-
+    /*
 
     if (location > 0.4) {
-        page = document.querySelector("#page2");
-        pageNumber = 1;
+        page = document.querySelector("#page1");
+        current = "#page0";
     } else if (location < 0.4 && location > -0.6) {
-        page = document.querySelector("#page2");
-        pageNumber = 2;
+        page = document.querySelector("#page1");
+        current = "#page1"; 
     } else if (location < -0.6) {
-        element = document.querySelector("#page3");
+        document.querySelector("#page2");
         //console.log("QSELECT PAGE 3")
         bounding = element.getBoundingClientRect();
         location = bounding.top/height;
 
         if (location < 0.4 && location > -0.6) {
-        pageNumber = 3;
+        //pageNumber = 2;
+        current = "#page2";
         } else {
-        pageNumber = 4;
+        //pageNumber = 3;
+        current = "#page3";
         }
 
     }
-    console.log(pages[0][pageNumber])
 
-    document.getElementById("header-selector").innerHTML = pages[0][pageNumber];
-    moveSelector(pages[1][pageNumber], pageNumber);
+    */
+    //console.log(pages[pageNumber]);
+
+    document.getElementById("header-selector").innerHTML = document.querySelector(`a[href="${current}"]`).innerHTML;
+    moveSelector(current);
 }
 
 
@@ -59,18 +67,30 @@ function headerWidth () {
    emptyWidth = contentWidth-linksWidth-100;
 }
 
-function moveSelector (moveInput, page) {
-    let moveTotal = emptyWidth + moveInput;
-    const link = document.querySelector(`a[href="#page${page}"]`);
-    
+function moveSelector (page) {
+    console.log(`a[href="${page}"]`)
+    const link = document.querySelector(`a[href="${page}"]`);
+    console.log(link)
+    //console.log(link.offsetParent);
+    console.log(link.offsetLeft)
+    slider.setAttribute('style', `transform: translateX(${link.offsetLeft-50-(slider.offsetWidth/2)+(link.offsetWidth/2)}px);`)
     //console.log(link);
-    console.log(moveTotal);
+   // console.log(moveTotal);
 }
 
-let page = document.querySelector("#page2");
+
+
 const headerContent = document.getElementById("header-content");
 
 window.addEventListener("resize", windowResize);
-window.addEventListener("resize", () => moveSelector(headerWidth(), pageNumber));
+//window.addEventListener("resize", () => moveSelector(10, 1));
 
 document.addEventListener("scroll", () => isInViewport(page, height, pages));
+
+document.querySelectorAll('.header ul a').forEach(element => {
+    //console.log(element[this]);
+    element.addEventListener('click', (e) => moveSelector(e.target.getAttribute('href')))
+    
+})
+
+//moveSelector(e.target.)
