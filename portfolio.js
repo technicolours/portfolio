2464a6f12b5b95409fdcdb48547a5e9dc24d1d77
AlexPine <alexlight.pineapple@gmail.com>
2023-08-01 17:32:06 -0400

    Make header indicator dynamic

--- portfolio.js
+++ portfolio.js
@@ -11,53 +11,69 @@ function windowResize() {
     console.log(height)
 }
 
-let page = document.querySelector("#page1");
+let page = document.querySelector("#page0");
 current = "#page0";
+let boundingArray = [];
 
-function isInViewport(element, height, array) {
-    //console.log(element);
+let elemList = document.querySelectorAll('#main>div');
+console.log(elemList[0].id);
+let elemArray = Array.from(elemList);
+// for (i = 0; i < elemArray.length; i++) {
+//     boundingArray[i] = elemArray[i].getBoundingClientRect();
+// }
 
-    let bounding = element.getBoundingClientRect();
-    //console.log(`"${current}"`)
-    const inViewport = document.querySelector(`${current}`);
-    //console.log(inViewport);
-    let location = bounding.top/inViewport.offsetHeight;
+/*
+function isInViewport(el) {
+const rect = el.getBoundingClientRect();
+// console.log(el.id + "bottom: " + bounding.bottom)
+// console.log(el.id + "top: " + Math.abs(bounding.top))
+//console.log("inner height" + window.innerHeight)
 
-    console.log(location);
-    //console.log(`Percentage: ${location}`)
+return (
+    rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+);
 
-    /*
-
-    if (location > 0.4) {
-        page = document.querySelector("#page1");
-        current = "#page0";
-    } else if (location < 0.4 && location > -0.6) {
-        page = document.querySelector("#page1");
-        current = "#page1"; 
-    } else if (location < -0.6) {
-        document.querySelector("#page2");
-        //console.log("QSELECT PAGE 3")
-        bounding = element.getBoundingClientRect();
-        location = bounding.top/height;
-
-        if (location < 0.4 && location > -0.6) {
-        //pageNumber = 2;
-        current = "#page2";
-        } else {
-        //pageNumber = 3;
-        current = "#page3";
-        }
-
-    }
-
-    */
-    //console.log(pages[pageNumber]);
-
-    document.getElementById("header-selector").innerHTML = document.querySelector(`a[href="${current}"]`).innerHTML;
-    moveSelector(current);
 }
 
+*/
 
+function isInViewport(el, percentVisible) {
+        let
+          rect = el.getBoundingClientRect(),
+          windowHeight = (window.innerHeight || document.documentElement.clientHeight);
+      
+        return !(
+          Math.floor(100 - (((rect.top >= 0 ? 0 : rect.top) / +-rect.height) * 100)) < percentVisible ||
+          Math.floor(100 - ((rect.bottom - windowHeight) / rect.height) * 100) < percentVisible
+        )
+}
+
+function locate(element, height, array) {
+
+    let inViewport = []
+
+    for (elem of elemList) {
+        if (isInViewport(elem, 60)) {
+            /*
+            inViewport.push(elem);
+            if (inViewport.length > 1) {
+                console.log('MORE THAN 1')
+                for (div of inViewport) {
+                    //console.log(inViewport[div]);
+                }
+                
+            }
+            */
+
+            moveSelector(`#${elem.id}`)
+            console.log(elem.id);
+        }
+    }
+    
+}
 
 function headerWidth () {
    const contentWidth = document.getElementById("header-content").offsetWidth;
@@ -68,14 +84,12 @@ function headerWidth () {
 }
 
 function moveSelector (page) {
-    console.log(`a[href="${page}"]`)
+    console.log(`a[href="${page}"]`);
     const link = document.querySelector(`a[href="${page}"]`);
-    console.log(link)
-    //console.log(link.offsetParent);
-    console.log(link.offsetLeft)
-    slider.setAttribute('style', `transform: translateX(${link.offsetLeft-50-(slider.offsetWidth/2)+(link.offsetWidth/2)}px);`)
-    //console.log(link);
-   // console.log(moveTotal);
+    //console.log(link.offsetLeft)    
+   document.getElementById("header-selector").innerHTML = document.querySelector(`a[href="${page}"]`).innerHTML;
+   let translate = link.offsetLeft-50-(slider.offsetWidth/2)+(link.offsetWidth/2);
+   slider.setAttribute('style', `transform: translateX(${translate}px);`)
 }
 
 
@@ -83,14 +97,17 @@ function moveSelector (page) {
 const headerContent = document.getElementById("header-content");
 
 window.addEventListener("resize", windowResize);
+
 //window.addEventListener("resize", () => moveSelector(10, 1));
 
-document.addEventListener("scroll", () => isInViewport(page, height, pages));
+document.addEventListener('a .within', () => moveSelector('#page1'));
+
+document.addEventListener("scrollend", () => locate(page, height, pages));
+
+
 
 document.querySelectorAll('.header ul a').forEach(element => {
-    //console.log(element[this]);
     element.addEventListener('click', (e) => moveSelector(e.target.getAttribute('href')))
     
-})
+});
 
-//moveSelector(e.target.)
